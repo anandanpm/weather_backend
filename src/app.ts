d83fa760079ed959d018463240b7364b1cfb07ed
anandanpm <ananda1732001@gmail.com>
2025-08-25 15:39:59 +0530

    (feat):Adding the cors implementation in the app.ts and also the change in the port in the server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import cors from "cors";
 import weatherRoutes from "./routes/weatherRoutes";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use("/weather", weatherRoutes);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
