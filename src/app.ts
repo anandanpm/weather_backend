a77ea96e91f5facd9eb1678247d1fbefa648f575
anandanpm <ananda1732001@gmail.com>
2025-08-26 10:29:48 +0530

    (cors):Adding the cors in the allowedHeaders in the cors

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.use(
     origin: process.env.FRONTEND_URL, 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
